949c970ba29037801cf2d2d7eb53ae58efb08108
Aliny Kelly <alinykellyfs@gmail.com>
2021-08-30 20:24:35 -0300

    Add files via upload

---
+++ Bootcamp_MRV/net e c sharp/DigitalInnovationOne/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nummeroDeVezes = 5;
+
+            for (int i=0; i < nummeroDeVezes; i++){
+                Console.WriteLine($"Bem-vindo ao curso de .NET (i)");
+            }
+        }
+    }
+}
